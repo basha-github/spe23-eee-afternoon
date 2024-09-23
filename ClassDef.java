e985103d38c7e7cfde46ed15070e869e861724c3
basha-github <sharifabee.alur@gmail.com>
2024-09-23 14:09:54 +0530

    first prg

--- ClassDef.java
+++ ClassDef.java
@@ -1,9 +1,47 @@
+class Student{
+	int id;
+	String name;
+	String college;
+	String course;
+	int fee;
+	@Override
+	public String toString() {
+		return "Student [id=" + id + "\n name=" + name + "\n college=" + college + 
+				"\n course=" + course + "\n fee=" + fee
+				+ "]";
+	}
+
+	
+	Student(int id,	String name,
+	String college,
+	String course,
+	int fee){
+		
+		this.id = id;
+		this.name = name;
+		this.college = college;
+		this.course = course;
+		this.fee = fee;
+		
+	}
+	
+}
+
+
 
 public class ClassDef {
 
 	public static void main(String[] args) {
 	
+		Student ramu = null;
+		ramu = new Student(123, "Ramu", "AVANTHI", "EEE", 35000);
 		
+		Student shyam = null;
+		shyam = new Student(124, "Shyam", "AVANTHI", "EEE", 45000);
+		
+		
+		System.out.println(ramu);
+		System.out.println(shyam);
 	}
 
 }
